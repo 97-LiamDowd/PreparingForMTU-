74a110a44c05752c69faa4171e05913cb27b0774
Liam Dowd <liam.jonathan.dowd@gmail.com>
2025-12-05 20:14:42 +0000

    Adjusting

--- src/fundamentals/HelloWorld.java
+++ src/fundamentals/HelloWorld.java
@@ -1,11 +1,11 @@
 package fundamentals;
 
-import javax.swing.JOptionPane;
-
 public class HelloWorld{
     public static void main(String[] args){
+
+        //the different outputs I currently know
         System.out.println("This is one way to say Hello, World!");
-        JOptionPane.showMessageDialog(null, "This is another, but more in your face!" + "\nHello, World!");
-        JOptionPane.showConfirmDialog(null, "Are you watching?");
-    }
-}
+    
+    }//end of main method
+
+}//end of class
