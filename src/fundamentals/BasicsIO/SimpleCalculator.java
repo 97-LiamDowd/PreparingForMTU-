2c2a14a6103bdd368f780ad4530750678badb5b9
Liam Dowd <liam.jonathan.dowd@gmail.com>
2025-12-06 13:52:35 +0000

    currently tring to figure out how to create the operators

--- src/fundamentals/BasicsIO/SimpleCalculator.java
+++ src/fundamentals/BasicsIO/SimpleCalculator.java
@@ -1,1 +1,30 @@
+package fundamentals.BasicsIO;
 
+import java.util.Scanner;
+
+public class SimpleCalculator{
+    public static void main(String[] args){
+        
+        //declaring variables
+        int num1, num2;
+        String choice1 = "+";
+        String choice2 = "-";
+        String choice3 = "*";
+        String choice4 = "/";
+        String operater = "0";
+        //for user input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Choose a number = ");
+        num1 = scanner.nextInt();
+
+        System.out.print("Choose between +, -, *, /");
+        operater = scanner.next();
+
+        System.out.print("Choose another number = ");
+        num2 = scanner.nextInt();
+
+
+    }//end of main method
+
+}//end of class
