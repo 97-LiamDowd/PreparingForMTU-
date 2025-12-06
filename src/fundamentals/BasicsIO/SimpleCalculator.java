70b2a70051ce954c35ce16dcf30923ef518743a6
Liam Dowd <liam.jonathan.dowd@gmail.com>
2025-12-06 13:55:40 +0000

    "taking a break"

--- src/fundamentals/BasicsIO/SimpleCalculator.java
+++ src/fundamentals/BasicsIO/SimpleCalculator.java
@@ -6,23 +6,26 @@ public class SimpleCalculator{
     public static void main(String[] args){
         
         //declaring variables
+        int sum;
         int num1, num2;
         String choice1 = "+";
         String choice2 = "-";
         String choice3 = "*";
         String choice4 = "/";
-        String operater = "0";
+        String operater;
         //for user input
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Choose a number = ");
         num1 = scanner.nextInt();
 
-        System.out.print("Choose between +, -, *, /");
+        System.out.print("Choose between +, -, *, / = ");
         operater = scanner.next();
 
         System.out.print("Choose another number = ");
         num2 = scanner.nextInt();
+
+        sum = num1 + operater + num2;
 
 
     }//end of main method
